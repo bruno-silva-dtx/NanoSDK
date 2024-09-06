96fde68812ee44bc3ae03dc1af4bafafc35afad1
bruno-silva-dtx <ex5629@uminho.pt>
2024-09-06 21:17:07 +0100

    Colocar o build

---
+++ demo/quic_mqtt_test/build/CMakeFiles/quic_client.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quic_client.dir/quic_client.c.o"
+  "CMakeFiles/quic_client.dir/quic_client.c.o.d"
+  "quic_client"
+  "quic_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/quic_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
